ca5e0980e636b588a37eb7fff7a36c3e5c975fc8
Joel Martins <86698896+joelpmartins@users.noreply.github.com>
2022-05-01 21:27:50 -0300

    Removido linhas comentadas

--- app.js
+++ app.js
@@ -13,38 +13,3 @@ app.use('/', routes)
 app.listen(PORT, function(){
     console.log(`Servidor foi iniciado na porta ${PORT}`)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*require('dotenv').config()
-
-const express = require('express')
-//const mongoose = require('mongoose')
-//const bcrypt = require('bcryptjs')
-//const jwt = require('jsonwebtoken')
-
-require('./database')
-
-const app = express()
-
-app.use(express.json())
-
-const User = require("./models/user.model")
-
-app.listen(3000, function(){
-    console.log(`Servidor foi iniciado na porta`)
-})*/
